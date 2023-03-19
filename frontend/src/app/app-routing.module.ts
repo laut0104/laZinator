8450d22b3669700ff4e08ba3111e99848364a4db
Miyu Yano <78589722+laut0104@users.noreply.github.com>
2023-03-19 13:45:07 +0900

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-import { ClothesListComponent } from './pages/clothes-list/clothes-list.component';
+// import { ClothesListComponent } from './pages/clothes-list/clothes-list.component';
 import { LiffInitComponent } from './pages/liff-init/liff-init.component';
 
 const routes: Routes = [
