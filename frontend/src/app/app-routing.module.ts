5b710fdfbeffe42f5a0de5e9842b200a217bc6d7
Miyu Yano <78589722+laut0104@users.noreply.github.com>
2023-03-19 13:39:27 +0900

    微調整

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+import { ClothesListComponent } from './pages/clothes-list/clothes-list.component';
 import { LiffInitComponent } from './pages/liff-init/liff-init.component';
 
 const routes: Routes = [
@@ -8,6 +9,11 @@ const routes: Routes = [
     path: '',
     canActivate: [AuthGuard],
     component: LiffInitComponent,
+  },
+  {
+    path: 'clothes-list',
+    canActivate: [AuthGuard],
+    component: ClothesListComponent,
   },
 ];
 
