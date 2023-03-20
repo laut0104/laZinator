52b4956fb9dee085d781e9f82bb0cdab699f668c
MAO-business <mao.f.shukatu@gmail.com>
2023-03-20 16:29:23 +0900

    HTMLファイル再作成
    HTMLファイルは再作成するために、一旦消去
    バックアップはとってある

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+import { ClothesAddComponent } from './pages/clothes-add/clothes-add.component';
 import { ClothesListComponent } from './pages/clothes-list/clothes-list.component';
 import { LiffInitComponent } from './pages/liff-init/liff-init.component';
 
@@ -10,6 +11,12 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     component: LiffInitComponent,
   },
+
+  {
+    path: 'cloth-add',
+    canActivate: [AuthGuard],
+    component: ClothesAddComponent,
+  }
   // {
   //   path: 'clothes-list',
   //   canActivate: [AuthGuard],
